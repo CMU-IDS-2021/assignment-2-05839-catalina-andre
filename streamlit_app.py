afd55f46de73d42ef6b6401bca1d9467422c6a6e
swiminspace <andre.j.michell2@gmail.com>
2021-03-07 19:54:41 -0500

    Additional ideas and a record of graphs
    Conducted some additional data exploration using datavoyager2.  Found
    ~12 postentially interesting correlations or lack thereof.  Also
    included two ideas for stories to tell within this dataset and offered
    an option for the more refined design.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,39 @@ st.write(chart)
 st.write(
     "Yikes, that isn't super helpful, everyone is bunched in the corner and the states are too numerous to be clearly "
     "visible! Maybe we could do some filtering?")
+
+# Observed Relationships in the data (generally at the state level seems to be appropriate for exploration
+"""
+cs_educ_ba (graduated from college) - cs_fam_wkidsinglemom (single mom)
+rel_tot (percent religious) - cs_fam_wkidsinglemom (single mom)
+cs_elf_ind_man (proportion working in manufacturing) - cs_educ_ba (graduated from college)
+cs_educ_ba (graduated from college) - cs_labforce (labor force participation)
+cs_educ_ba (graduated from college) - hhinc00 (household income)
+cs_educ_ba (graduated from college) - poor_share (share of pop in poverty)
+cs_educ_ba (graduated from college) - tuition (cost of tuition) (interesting b/c no relationship)
+subcty_exp_pc (local gov't spending) - taxrate (local government tax rate)
+cs_born_foreign (share of pop foreign born) - crime_total (local crime rate) (no relationship observed)
+dropout_r (high school dropouts) - crime_total (crime rate) (some states have strong relationship, others not so much)
+
+General Ideas for a story to tell with the Data:
+- Confronting Stereotypes
+  - explore and share if this data supports stereotypes about health, education, crime, etc.
+  - one interesting one to consider is surrounding percentage of foreign born data
+    - less crime, lower obesity, fewer single moms, less smoking, fewer high school dropouts
+      generally are moving to areas others are moving away from, 
+- Why are they leaving?
+  - Consider areas people are migrating away from and show how those areas differ the most from
+    areas people are staying in or moving to
+  - again misconceptions could be interesting, like considering household income, home value, crime,
+    education, etc.
+
+Our design in greater detail:
+- A single map showing the entire US with charts showing aggregate statistics on the right
+  or maybe under the main plot (configurable displays, but maybe only show set combinations)
+- The ability to compare two states with a choropleth display of the state's outline and a
+  selected feature to compare more broadly.
+    - sub-plots below compare the states more directly in different areas.
+- Can guide a viewer through a story to expose differences between the Northeast and Southeast,
+  mid-west and south-west, and/or the pacific north-west
+
+"""
