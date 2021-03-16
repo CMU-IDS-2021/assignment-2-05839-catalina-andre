d799cbd2c7ce8522245bf39908506978c4c38320
Catalina Vajiac <catvajiac@gmail.com>
2021-03-16 14:29:58 -0400

    start final version

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,25 @@ from sklearn.manifold import TSNE
 
 from vega_datasets import data
 
+# mapping the features in our df that interest us to nice strings for vis
+feature_titles = [
+  ('puninsured2010', '% uninsured'),
+  ('poor_share', 'poverty rate'),
+  ('cs_frac_black', '% black'),
+  ('cs_frac_hisp', '% hispanic'),
+  ('unemp_rate', 'unemployment rate'),
+  ('cs_born_foreign', '% foreign-born'),
+  ('hhinc00', 'avg. household income'),
+  ('cs_educ_ba', '% with Bachelors degree'),
+  ('crime_total', 'crime rate')
+]
+
+title_to_feature = {b: a for a, b in feature_titles}
+feature_to_title = {a: b for a, b in feature_titles}
 
 @st.cache
 def load_geodata():
+  ''' load topographical data for US county data '''
   return alt.topo_feature(
     "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/united-states/us-albers-counties.json",
     "collection"
@@ -18,6 +34,7 @@ def load_geodata():
 
 @st.cache
 def clustering_visual(df):
+  ''' perform KMeans clustering on features '''
   feature_matrix = df.select_dtypes(include='number').to_numpy(na_value=0)
   clustering = KMeans().fit(feature_matrix)
   tsne_embed = TSNE().fit_transform(feature_matrix)
@@ -36,35 +53,38 @@ def load_data():
     df['fips'] = ['{:05}'.format(num) for num in df.cty]
     return df
 
-@st.cache
-def load_counties():
-    return alt.topo_feature(data.us_10m.url, 'counties')
-
 
 def choro_map(geo_data, df, column, scale):
-  tooltip_cols = ('properties.name:O', 'properties.state:O', '{}:Q'.format(column))
-  chart = alt.Chart(geo_data).mark_geoshape(
+  ''' render choropleth map using topo layout from @geo_data,
+      colored by feature @column in data @df, using linear or log @scale '''
+  def get_title(s): # get display title for tooltips
+    s = s.split(':')[0]
+    if s.startswith('properties'):
+      s = s.split('.')[1]
+    return feature_to_title.get(s, s)
+
+  col_feature = title_to_feature.get(column, column)
+  tooltip_cols = ('properties.name:O', 'properties.state:O', '{}:Q'.format(col_feature))
+
+  return alt.Chart(geo_data).mark_geoshape(
   ).encode(
-    color=alt.Color('{}:Q'.format(column), scale=alt.Scale(type=scale, scheme='greenblue')),
-    tooltip=[alt.Tooltip(col, title=col.split(':')[0]) for col in tooltip_cols]
+    color=alt.Color('{}:Q'.format(col_feature), title=column, scale=alt.Scale(type=scale, scheme='greenblue')),
+    tooltip=[alt.Tooltip(col, title=get_title(col)) for col in tooltip_cols]
   ).properties(
-    width=1000,
+    width=800,
     height=600
   ).transform_lookup(
     lookup='properties.fips',
-    from_=alt.LookupData(df, "fips", [column])
+    from_=alt.LookupData(df, "fips", [col_feature])
   ).project(
     'albersUsa'
   )
 
-  return chart
 
-
-def feature_dropdown(s, key, default_val):
-  remove_cols = ('fips', 'cz', 'state_id', 'stateabbrv', 'csa', 'cbsa', 'intersects_msa')
-  keep_cols = [c for c in df.columns if 'name' not in c and c not in remove_cols]
-  default_index = keep_cols.index(default_val)
-  return st.selectbox(s, keep_cols, key=key, index=default_index)
+def feature_dropdown(s, key, default_val, features):
+  get_title = lambda x: feature_to_title[x] if x in feature_to_title else x
+  default_index = features.index(default_val)
+  return st.selectbox(s, [get_title(f) for f in features], key=key, index=default_index)
 
 
 def scale_dropdown(key):
@@ -85,12 +105,34 @@ def pair_plot(df, x_str, y_str, scale_str, tooltip_cols=[]):
   )
 
 
-st.title("Let's analyze some Social Mobility Data 📊.")
+st.title("Confronting Stereotypes in the Immigration Debate.")
 df = load_data()
 
-st.write("Let's look at the raw data in a Pandas Data Frame.")
 
-st.write(df)
+st.write('pair plots go here.')
+
+
+
+st.write('How do these factors look like individually, across the US?')
+
+choro_features= ['puninsured2010', 'poor_share', 'cs_frac_black', 'cs_frac_hispanic', 'unemp_rate',
+  'cs_born_foreign', 'hhinc00', 'cs_educ_ba', 'crime_total']
+#choro_features = list(df.columns)
+feature = feature_dropdown('Which feature would you like to look at?', 'choro_feature', 'crime_total', choro_features)
+scale = scale_dropdown('choro_scale')
+
+counties_data = load_geodata()
+st.write(choro_map(counties_data, df, feature, scale))
+
+state = st.selectbox('Would you like to zoom in on any particular state?',
+  ['None'] + list(df.statename.unique()))
+
+if state != 'None':
+  st.write(choro_map(counties_data, df[df.statename == state], feature, scale))
+
+
+
+st.write('ALL OLD STUFF BELOW HERE: EVENTUALLY DELETE FOR FINAL VERSION')
 
 st.write(
     "Whew, that's a lot of columns!  Hmm 🤔, is there some correlation between the population and median house value? "
@@ -130,8 +172,8 @@ st.write(chart)
 st.write(df.median_house_value)
 
 scale = scale_dropdown('pair_plot_scale')
-x_str = feature_dropdown('Choose x-axis feature.', 'x_feature', 'cty_pop2000')
-y_str = feature_dropdown('Choose y-axis feature.', 'y_feature', 'median_house_value')
+x_str = feature_dropdown('Choose x-axis feature.', 'x_feature', 'cty_pop2000', list(df.columns))
+y_str = feature_dropdown('Choose y-axis feature.', 'y_feature', 'median_house_value', list(df.columns))
 st.write(pair_plot(df, x_str, y_str, scale, [x_str, y_str, 'county_name', 'statename']))
 
 
@@ -144,39 +186,6 @@ st.write(pair_plot(agg_df, x_str, y_str, scale, [x_str, y_str, 'statename']))
 
 st.write('This is less crowded, though we lose some information.')
 
-tsne_df = clustering_visual(df)
-
-chart = alt.Chart(tsne_df).mark_point().encode(
-    x=alt.X("tsne_x"),
-    y=alt.Y("tsne_y"),
-    color=alt.Color("cluster:N")
-).properties(
-    width=600, height=400
-)
-
-st.write(chart)
-
-
-st.write("What I've done above is running kmeans clustering on all numeric data. We can probably find a better clustering",
-"But, the point is to answer 'Which counties are similar? We might find some interesting relationships this way.")
-
-
-counties = load_counties()
-
-counties_map = alt.Chart(counties).mark_geoshape().encode(
-    tooltip=["hhinc00:Q", 'county_name:N', 'statename:N'],
-    color='hhinc00:Q'
-).transform_lookup(
-    lookup='id',
-    from_=alt.LookupData(df, 'cty', ['hhinc00', 'county_name', 'statename'])
-).project(
-    type='albersUsa'
-).properties(
-    width=700,
-    height=500
-)
-
-st.write(counties_map)
 
 # Observed Relationships in the data (generally at the state level seems to be appropriate for exploration
 """
@@ -220,17 +229,3 @@ Our design in greater detail:
   A map from state to region can be found here: https://github.com/cphalpert/census-regions/blob/master/us%20census%20bureau%20regions%20and%20divisions.csv.
 
 """
-
-counties_data = load_geodata()
-
-feature = feature_dropdown('Which feature would you like to look at?', 'choro_feature', 'cty_pop2000')
-
-scale = scale_dropdown('choro_scale')
-
-st.write(choro_map(counties_data, df, feature, scale))
-
-state = st.selectbox('Would you like to zoom in on any particular state?',
-  ['None'] + list(df.statename.unique()))
-
-if state != 'None':
-  st.write(choro_map(counties_data, df[df.statename == state], feature, scale))
